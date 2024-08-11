0aaf3ca31cc960c18fb8d13d17df429c36681242
minhduc63tg <minhduc.63tg@gmail.com>
2024-08-11 16:58:30 +0700

    First commit

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Le Minh Duc");
+        System.out.println("Welcome to the DHKTPM19A!");
+    }
+}
